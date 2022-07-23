3ea6137a696cd203c567369025289c5f75ee369a
Tomer Harari <tharari21@gmail.com>
2022-07-23 15:29:07 -0400

    commit tree 1012edfed1595a3473b87ec3afbb9d27f34ba9a2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Search from "./Search";
 
-function Header() {
+function Header({ searchValue, setSearchValue }) {
   return (
     <header>
       <h1>
@@ -10,7 +10,7 @@ function Header() {
         </span>
         gregslist
       </h1>
-      <Search />
+      <Search searchValue={searchValue} setSearchValue={setSearchValue} />
     </header>
   );
 }
